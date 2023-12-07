28becd8d6614cba4e96c721764e2fe8c326f78b3
pauljo13 <pauljbs1397@gmail.com>
2023-12-08 00:01:05 +0900

    2023-12-07

---
+++ JavaWorkspace/src/chap_01/_04_Comment.java
@@ -0,0 +1,20 @@
+package chap_01;
+
+public class _04_Comment {
+    public static void main(String[] args) {
+        System.out.println("(10분 전) 잠시 후 결혹식 시작 예정이오니 착섯 부탁드립니다. ");
+        //System.out.println("(5분 전) 잠시 후 결혹식 시작 예정이오니 착섯 부탁드립니다. ");
+        System.out.println("지금부터 결혼식을 시작하겠습니다.");
+
+        int size = 120;
+        size = size + 10; // 어린이에 경우는 성장을 빠르기 떄문에 사이즈 10만큼 더 큰 신발을 구매한다.
+        System.out.println("신발 사이즈 " + size + "으로 보여주세요.");
+
+        // 한 줄 수석 단축키 : Command + /
+        // 여러 줄 주석 단축키 : Command + Shift + /
+        /*int a = 10;
+        int b = 20;
+        System.out.println(a + b);*/
+
+    }
+}
